d0a705970ca3c491971c7ae1ce45faa6aaea6718
Mason Remaley <mason@anthropicstudios.com>
2025-06-05 16:30:09 -0700

    More color functions

--- include/gbms/color.glsl
+++ include/gbms/color.glsl
@@ -1,6 +1,8 @@
 #ifndef INCLUDE_GBMS_COLOR
 #define INCLUDE_GBMS_COLOR
 
+#include "constants.glsl"
+
 float colorLinearToSrgb(float linear) {
     // The color component transfer function from the SRGB specification:
     // https://www.w3.org/Graphics/Color/srgb
@@ -103,6 +105,11 @@ uvec4 colorFloatToUnorm(vec4 f) {
 }
 
 
+// Converts linear to OkLab, a perceptual color space.
+//
+// Lab = (Lightness, green/red, blue/yellow)
+//
+// Adapted from: https://bottosson.github.io/posts/oklab/#converting-from-linear-srgb-to-oklab
 vec3 colorLinearToOklab(vec3 c)  {
     vec3 lms = c * mat3(
         vec3(0.4122214708, 0.5363325363, 0.0514459929),
@@ -139,8 +146,80 @@ vec3 colorOklabToLinear(vec3 c)  {
     );
 }
 
+// Converts from OkLab to linear. See `colorLinearToOklab
+//
+// Adapted from: https://bottosson.github.io/posts/oklab/#converting-from-linear-srgb-to-oklab
 vec4 colorOklabToLinear(vec4 c)  {
-    return vec4(colorOklabToLinear(c.rgb), c.a);
+    return vec4(colorOklabToLinear(c.xyz), c.w);
+}
+// Finds the maximum saturation possible for a given hue that fits in sRGB
+// Saturation here is defined as S = C/L
+// a and b must be normalized so a^2 + b^2 == 1
+//
+// Adapted from: https://bottosson.github.io/posts/gamutclipping/
+
+// Returns the maximum saturation for the hue `ab` that fits in sRGB.
+//
+// Saturation is `C/L`, `ab` must be normalized. 
+//
+// Adapted from here: https://bottosson.github.io/posts/gamutclipping/#intersection-with-srgb-gamut
+float colorOklabMaxSaturation(vec2 ab) {
+    // Max saturation will be when one of r, g or b goes below zero.
+
+    // Select different coefficients depending on which component goes below zero first
+    float k[5];
+    vec3 w_lms;
+
+    if (dot(ab, vec2(-1.88170328, -0.80936493)) > 1) {
+        k = float[5](
+            +1.19086277,
+            +1.76576728,
+            +0.59662641,
+            +0.75515197,
+            +0.56771245
+        );
+        w_lms = vec3(4.0767416621, -3.3077115913, 0.2309699292);
+    } else if (dot(ab, vec2(1.81444104, -1.19445276)) > 1) {
+        k = float[5](
+            +0.73956515,
+            -0.45954404,
+            +0.08285427,
+            +0.12541070,
+            +0.14503204
+        );
+        w_lms = vec3(-1.2684380046, 2.6097574011, -0.3413193965);
+    } else {
+        k = float[5](
+            +1.35733652,
+            -0.00915799,
+            -1.15130210,
+            -0.50559606,
+            +0.00692167
+        );
+        w_lms = vec3(-0.0041960863, -0.7034186147, 1.7076147010);
+    }
+
+    float S = k[0] + k[1] * ab.x + k[2] * ab.y + k[3] * ab.x * ab.x + k[4] * ab.x * ab.y;
+
+    vec3 k_lms = ab * mat3x2(
+        vec2(+0.3963377774, +0.2158037573),
+        vec2(-0.1055613458, -0.0638541728),
+        vec2(-0.0894841775, -1.2914855480)
+    );
+
+    {
+        vec3 lms_p1 = fma(vec3(S), k_lms, vec3(1.0));
+        vec3 lms_p2 = lms_p1 * lms_p1;
+        vec3 lms = lms_p2 * lms_p1;
+
+        vec3 lms_dS = 3.0 * k_lms * lms_p2;
+        vec3 lms_ds2 = 6.0 * k_lms * k_lms * lms_p1;
+        vec3 f = w_lms * mat3(lms, lms_dS, lms_ds2);
+
+        S = fma(-f.x, f.y / fma(f.y, f.y, -0.5 * f.x * f.z), S);
+    }
+
+    return S;
 }
 
 #endif
