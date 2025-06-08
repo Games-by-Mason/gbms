32f9b08c8970716f496878335dfae5e37eacfddc
Mason Remaley <mason@anthropicstudios.com>
2025-06-08 01:54:28 -0700

    Cleans up unpacking, there are some builtins for this
    Mimics the builtin naming scheme where coverage is missing

--- include/gbms/unpack.glsl
+++ include/gbms/unpack.glsl
@@ -1,16 +1,7 @@
 #ifndef INCLUDE_GBMS_UNPACK
 #define INCLUDE_GBMS_UNPACK
 
-uvec4 uintToUvec4(uint u) {
-	return uvec4(
-	    (u & 0xFF000000) >> 24,
-	    (u & 0x00FF0000) >> 16,
-	    (u & 0x0000FF00) >> 8,
-	    (u & 0x000000FF)
-	);
-}
-
-ivec2 uintToUvec2(uint n) {
+ivec2 unpackShort2x16(uint n) {
 	return ivec2(
 		n & 0x0000FFFF,
 		n >> 16
