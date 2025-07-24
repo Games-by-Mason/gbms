7d07c566a35c4f6fd105c40196966bcc6925d2c2
Mason Remaley <mason@anthropicstudios.com>
2025-07-24 15:56:19 -0700

    Cleans up the C interface a bit for sharing compute shader dispatch sizes

--- include/gbms/c.glsl
+++ include/gbms/c.glsl
@@ -13,35 +13,55 @@
 	#define EXPORT_STRUCT(name)
 #endif
 
-// Macros for C versions of GLSL primitives
+// Macros & typedefs for matching C and GLSL types
 #ifdef __STDC__
 	#include <stdint.h>
 
-	#define bool uint32_t
-	#define u32 uint32_t
-	#define i32 int32_t
-	#define f32 float
-	#define f64 double
+	typedef uint32_t bool;
+	typedef uint32_t u32;
+	typedef int32_t i32;
+	typedef float f32;
+	typedef double f64;
 
-	typedef bool bvec2[2];
-	typedef bool bvec3[3];
-	typedef bool bvec4[4];
+	typedef struct { bool x; bool y; } bvec2;
+	typedef struct { bool x; bool y; bool z; } bvec3;
+	typedef struct { bool x; bool y; bool z; bool w; } bvec4;
 
-	typedef i32 ivec2[2];
-	typedef i32 ivec3[3];
-	typedef i32 ivec4[4];
+	#define bvec2(x, y) (bvec2){ x, y }
+	#define bvec3(x, y, z) (bvec3){ x, y, z }
+	#define bvec4(x, y, z, w) (bvec3){ x, y, z, w }
 
-	typedef u32 uvec2[2];
-	typedef u32 uvec3[3];
-	typedef u32 uvec4[4];
+	typedef struct { i32 x; i32 y; } ivec2;
+	typedef struct { i32 x; i32 y; i32 z; } ivec3;
+	typedef struct { i32 x; i32 y; i32 z; i32 w; } ivec4;
 
-	typedef f32 vec2[2];
-	typedef f32 vec3[3];
-	typedef f32 vec4[4];
+	#define ivec2(x, y) (ivec2){ x, y }
+	#define ivec3(x, y, z) (ivec3){ x, y, z }
+	#define ivec4(x, y, z, w) (ivec3){ x, y, z, w }
 
-	typedef f64 dvec2[2];
-	typedef f64 dvec3[3];
-	typedef f64 dvec4[4];
+	typedef struct { u32 x; u32 y; } uvec2;
+	typedef struct { u32 x; u32 y; u32 z; } uvec3;
+	typedef struct { u32 x; u32 y; u32 z; u32 w; } uvec4;
+
+	#define uvec2(x, y) (uvec2){ x, y }
+	#define uvec3(x, y, z) (uvec3){ x, y, z }
+	#define uvec4(x, y, z, w) (uvec3){ x, y, z, w }
+
+	typedef struct { f32 x; f32 y; } vec2;
+	typedef struct { f32 x; f32 y; f32 z; } vec3;
+	typedef struct { f32 x; f32 y; f32 z; f32 w; } vec4;
+
+	#define vec2(x, y) (vec2){ x, y }
+	#define vec3(x, y, z) (vec3){ x, y, z }
+	#define vec4(x, y, z, w) (vec3){ x, y, z, w }
+
+	typedef struct { f64 x; f64 y; } dvec2;
+	typedef struct { f64 x; f64 y; f64 z; } dvec3;
+	typedef struct { f64 x; f64 y; f64 z; f64 w; } dvec4;
+
+	#define dvec2(x, y) (dvec2){ x, y }
+	#define dvec3(x, y, z) (dvec3){ x, y, z }
+	#define dvec4(x, y, z, w) (dvec3){ x, y, z, w }
 
 	typedef f32 mat2x2[4];
 	typedef f32 mat2x3[6];
@@ -61,4 +81,11 @@
 	#define f64 double
 #endif
 
+// Math functions that are shared between C and GLSL
+u32 divCeil(u32 a, u32 b) {
+	if (a == 0 || b == 0) return 0;
+	return ((a - 1u) / b) + 1u;
+}
+
 #endif
+
