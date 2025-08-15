d8d31307002dac414370b73b5fa6ea636a2158cf
Mason Remaley <mason@anthropicstudios.com>
2025-08-14 21:53:04 -0700

    Adds ab mode to latency test shader

--- include/gbms/debug.glsl
+++ include/gbms/debug.glsl
@@ -7,14 +7,16 @@
 #include "c.glsl"
 
 // Draws a latency and smoothness test pattern. Includes timed elements, and elements that react to
-// mouse position and movement.
+// mouse position and movement. `ab` can be used to specify which side of an AB test is being
+// displayed, setting it to `true` will display `a` and `false` will display `b`.
 vec4 debugLatency(
 	vec2 p,
 	vec2 size,
 	vec2 mouse_position,
 	u32 mouse_buttons,
 	float seconds,
-	float speed
+	float speed,
+	bool ab
 ) {
     if (mouse_buttons != 0) return vec4(1);
 	float line2 = sdBox(
@@ -23,6 +25,20 @@ vec4 debugLatency(
 	);
 	float box = sdBox(p - mouse_position, vec2(20, 20));
 	float sd = opUnion(line2, box);
+
+	if (ab) {
+		sd = opUnion(sd, sdBox(p - vec2(40, 40), vec2(5, 20))); // Left vert
+		sd = opUnion(sd, sdBox(p - vec2(60, 40), vec2(5, 20))); // Right vert
+		sd = opUnion(sd, sdBox(p - vec2(45, 40), vec2(10, 5))); // Center horiz
+		sd = opUnion(sd, sdBox(p - vec2(50, 20), vec2(10, 5))); // Top horiz
+	} else {
+		sd = opUnion(sd, sdBox(p - vec2(40, 40), vec2(5, 20))); // Left vert
+		sd = opUnion(sd, sdBox(p - vec2(60, 40), vec2(5, 20))); // Right vert
+		sd = opUnion(sd, sdBox(p - vec2(45, 40), vec2(10, 5))); // Center horiz
+		sd = opUnion(sd, sdBox(p - vec2(45, 20), vec2(15, 5))); // Top horiz
+		sd = opUnion(sd, sdBox(p - vec2(45, 60), vec2(15, 5))); // Bottom horiz
+	}
+
 	return vec4(vec3(step(0, -sd)), 1);
 }
 
