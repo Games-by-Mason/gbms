00da87f556ed042d66155a3421c5d46ecc98b68b
Mason Remaley <mason@anthropicstudios.com>
2025-09-08 22:29:28 -0700

    Allows skipping the a/b debug viz

--- include/gbms/debug.glsl
+++ include/gbms/debug.glsl
@@ -16,7 +16,7 @@ vec4 debugLatency(
 	u32 mouse_buttons,
 	float seconds,
 	float speed,
-	bool ab
+	u32 ab
 ) {
     if (mouse_buttons != 0) return vec4(1);
 	float line2 = sdBox(
@@ -26,17 +26,21 @@ vec4 debugLatency(
 	float box = sdBox(p - mouse_position, vec2(20, 20));
 	float sd = opUnion(line2, box);
 
-	if (ab) {
-		sd = opUnion(sd, sdBox(p - vec2(40, 40), vec2(5, 20))); // Left vert
-		sd = opUnion(sd, sdBox(p - vec2(60, 40), vec2(5, 20))); // Right vert
-		sd = opUnion(sd, sdBox(p - vec2(45, 40), vec2(10, 5))); // Center horiz
-		sd = opUnion(sd, sdBox(p - vec2(50, 20), vec2(10, 5))); // Top horiz
-	} else {
-		sd = opUnion(sd, sdBox(p - vec2(40, 40), vec2(5, 20))); // Left vert
-		sd = opUnion(sd, sdBox(p - vec2(60, 40), vec2(5, 20))); // Right vert
-		sd = opUnion(sd, sdBox(p - vec2(45, 40), vec2(10, 5))); // Center horiz
-		sd = opUnion(sd, sdBox(p - vec2(45, 20), vec2(15, 5))); // Top horiz
-		sd = opUnion(sd, sdBox(p - vec2(45, 60), vec2(15, 5))); // Bottom horiz
+	switch (ab) {
+		case 1: {
+			sd = opUnion(sd, sdBox(p - vec2(40, 40), vec2(5, 20))); // Left vert
+			sd = opUnion(sd, sdBox(p - vec2(60, 40), vec2(5, 20))); // Right vert
+			sd = opUnion(sd, sdBox(p - vec2(45, 40), vec2(10, 5))); // Center horiz
+			sd = opUnion(sd, sdBox(p - vec2(50, 20), vec2(10, 5))); // Top horiz
+		} break;
+		case 2: {
+			sd = opUnion(sd, sdBox(p - vec2(40, 40), vec2(5, 20))); // Left vert
+			sd = opUnion(sd, sdBox(p - vec2(60, 40), vec2(5, 20))); // Right vert
+			sd = opUnion(sd, sdBox(p - vec2(45, 40), vec2(10, 5))); // Center horiz
+			sd = opUnion(sd, sdBox(p - vec2(45, 20), vec2(15, 5))); // Top horiz
+			sd = opUnion(sd, sdBox(p - vec2(45, 60), vec2(15, 5))); // Bottom horiz
+		} break;
+
 	}
 
 	return vec4(vec3(step(0, -sd)), 1);
