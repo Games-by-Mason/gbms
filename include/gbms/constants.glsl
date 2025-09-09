5f630dff84406f1480b4cf0858322b097aaaae10
Mason Remaley <mason@anthropicstudios.com>
2025-09-08 18:59:57 -0700

    Makes it possible to use constants in C interface

--- include/gbms/constants.glsl
+++ include/gbms/constants.glsl
@@ -3,11 +3,28 @@
 
 #include "c.glsl"
 
-f32 U32_MAX_RECIP = 1.0f / f32(0xFFFFFFFFu);
-u32 U32_MAX = 0xFFFFFFFF;
-f32 F32_MAX_CONSEC = 16777216.0;
-f32 INF = 1.0 / 0.0;
-f32 PI = 3.14159265358979323846264338327950288419716939937510;
-f32 F32_MAX = 3.40282347E+38;
+#ifdef __STDC__
+	const u32 U32_MAX = (u32)0xFFFFFFFF;
+#else
+	const u32 U32_MAX = 0xFFFFFFFF;
+#endif
+
+#ifdef __STDC__
+	const f32 U32_MAX_RECIP = 1.0f / (f32)0xFFFFFFFFu;
+#else
+	const f32 U32_MAX_RECIP = 1.0f / f32(0xFFFFFFFFu);
+#endif
+
+const f32 F32_MAX_CONSEC = 16777216.0;
+
+#ifdef __STDC__
+	#include <math.h>
+	const f32 INF = INFINITY;
+#else
+	const f32 INF = 1.0 / 0.0;
+#endif
+
+const f32 PI = 3.14159265358979323846264338327950288419716939937510;
+const f32 F32_MAX = 3.40282347E+38;
 
 #endif
